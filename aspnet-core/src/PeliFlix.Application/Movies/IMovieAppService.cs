839836c08e0c48a624f1c2ed74e469344b2c9f6d
Jhon Tejada <jhontejada13@gmail.com>
2020-12-02 22:05:07 -0500

    Se modifica la interface de Movie

---
+++ aspnet-core/src/PeliFlix.Application/Movies/IMovieAppService.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services;
+using PeliFlix.Entities;
+using PeliFlix.Movies.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliFlix.Movies
+{
+    public interface IMovieAppService : IApplicationService
+    {
+        Task<List<Movie>> GetAll();
+        //List<MovieDto> GetAll();
+    }
+}
