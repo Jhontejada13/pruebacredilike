5711a41c8f5577f63cfd150a2eab1c38550f6fe5
Jhon Tejada <jhontejada13@gmail.com>
2020-12-02 22:02:19 -0500

    Se modifican el .html y el .ts del componente de movies

--- angular/src/app/movies/movies.component.ts
+++ angular/src/app/movies/movies.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+import {
+  PagedListingComponentBase,
+  PagedRequestDto
+} from 'shared/paged-listing-component-base';
+import { MovieDto, MovieServiceProxy } from '@shared/service-proxies/service-proxies';
+
+class PagedUsersRequestDto extends PagedRequestDto {
+  keyword: string;
+  isActive: boolean | null;
+}
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent implements OnInit {
+export class MoviesComponent extends PagedListingComponentBase<MovieDto>{
+  protected list(request: PagedRequestDto, pageNumber: number, finishedCallback: Function): void {
+    throw new Error('Method not implemented.');
+  }
+  movies: MovieDto[] = [];
+  keyword = '';
+  isActive: boolean | null;
+  advancedFiltersVisible = false;
 
-  constructor() { }
+  constructor(
+    injector: Injector,
+    private _moviesService: MovieServiceProxy,
+    private _modalService: BsModalService
+  ) { 
+    super(injector);
+  }
 
   ngOnInit(): void {
+  }
+
+  protected delete(movie: MovieDto): void {
+    abp.message.confirm(
+      this.l('MovieDeleteWarningMessage', movie.movieName),
+      undefined,
+      (result: boolean) => {
+        if (result) {
+          this._moviesService.delete(movie.id).subscribe(() => {
+            abp.notify.success(this.l('SuccessfullyDeleted'));
+            this.refresh();
+          });
+        }
+      }
+    );
   }
 
 }
