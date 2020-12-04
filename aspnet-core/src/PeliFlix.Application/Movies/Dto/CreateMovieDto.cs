84509384d31947929df44ecb70a79b84b9888991
Jhon Tejada <jhontejada13@gmail.com>
2020-12-04 17:06:48 -0500

    Se actualiza la clase eliminando un campo innecesario y asegurar el correcto funcionamiento del método UpdateAsync del servicio

---
+++ aspnet-core/src/PeliFlix.Application/Movies/Dto/CreateMovieDto.cs
@@ -0,0 +1,42 @@
+﻿using Abp.AutoMapper;
+using Abp.Runtime.Validation;
+using PeliFlix.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliFlix.Movies.Dto
+{
+    [AutoMapTo(typeof(Movie))]
+    public class CreateMovieDto : IShouldNormalize
+    {
+
+        [Required]
+        //[StringLength(maximumLength:300)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Synopsis { get; set; }
+
+        [Required]
+        public int year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        //public string[] GenderNames { get; set; }
+        [Required]
+        public int GenderId { get; set; }
+
+        public void Normalize()
+        {
+            //if (GenderNames == null)
+            //{
+            //    GenderNames = new string[0];
+            //}
+        }
+    }
+}
