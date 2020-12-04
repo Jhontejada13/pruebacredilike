8f34e4513425ad3952d662f2402f1a6bc92b5400
Jhon Tejada <jhontejada13@gmail.com>
2020-12-04 17:05:20 -0500

    Se modifica la clase para asegurar que se envien los datos necesario

---
+++ aspnet-core/src/PeliFlix.Application/Movies/Dto/PagedMovieRequestResultDto.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliFlix.Movies.Dto
+{
+    public class PagedMovieRequestResultDto : PagedResultRequestDto
+    {
+        public string keyword { get; set; } //Para filtrar y buscar por algún campo ... 
+    }
+}
