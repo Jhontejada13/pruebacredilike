be514b5562f03d9fd3622633eb669929d6ce9138
Jhon Tejada <jhontejada13@gmail.com>
2020-12-10 19:55:48 -0500

    Se realiza el código para modificar películas

--- angular/src/app/movies/edit-movie/edit-movie-dialog.component.ts
+++ angular/src/app/movies/edit-movie/edit-movie-dialog.component.ts
@@ -12,6 +12,7 @@ import {
   import {
     MovieServiceProxy,
     MovieDto,
+    GenderDto
   } from '@shared/service-proxies/service-proxies';
 
   @Component({
@@ -22,6 +23,9 @@ import {
   implements OnInit {
   saving = false;
   movie = new MovieDto();
+  genders: GenderDto[] = [];
+  genderSelected: GenderDto | null;
+  chekedGendersMap: { [key: string]: boolean } = {};
   id: number;
 
   @Output() onSave = new EventEmitter<any>();
@@ -37,12 +41,46 @@ import {
   ngOnInit(): void {
     this._movieService.get(this.id).subscribe((result) => {
       this.movie = result;
+
+      this._movieService.getGenders().subscribe((resultTwo) => {
+        this.genders = resultTwo.items
+        
+      });
     });
+  }
+
+  setInitialGendersStatus(): void {
+    _map(this.genders, (item) => {
+      this.chekedGendersMap[item.name] = this.isGenderCheked(
+        item.name
+      )
+    })
+  }
+
+  isGenderCheked(name: string): boolean{
+    return _includes(this.movie.gendersName, name);
+  }
+
+  onGenderChange(gender: GenderDto, $event){
+    this.chekedGendersMap[gender.name] = $event.target.checked;
+  }
+
+  getChekedGenders(): string[]{
+    const genders: string[] = [];
+    _forEach(this.chekedGendersMap, function (value, key) {
+      if(value){
+        genders.push(key);
+      }
+    });
+    return genders;
   }
 
   save(): void {
     this.saving = true;
 
+    this.movie.gendersName = this.getChekedGenders();
+
+    console.log(this.movie);
     this._movieService
       .update(this.movie)
       .pipe(
