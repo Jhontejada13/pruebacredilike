d48d93b81724d111f58f40e1dafef2e03fb86d7c
Jhon Tejada <jhontejada13@gmail.com>
2020-12-11 18:56:21 -0500

    Se crea el método para obtener la información de movie junto a la info de mivie_seen

--- aspnet-core/src/PeliFlix.Application/Movies_Seen/MovieSeenAppService.cs
+++ aspnet-core/src/PeliFlix.Application/Movies_Seen/MovieSeenAppService.cs
@@ -3,6 +3,7 @@ using Abp.Application.Services.Dto;
 using Abp.Domain.Repositories;
 using Abp.UI;
 using PeliFlix.Entities;
+using PeliFlix.Movies.Dto;
 using PeliFlix.Movies_Seen.Dto;
 using System;
 using System.Collections.Generic;
@@ -10,16 +11,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace PeliFlix.Movies_Seen
 {
     public class MovieSeenAppService : AsyncCrudAppService<Movie_Seen, Movie_SeenDto, int, PagedMovieSeenRequestResultDto,
         CreateMovie_SeenDto, Movie_SeenDto>, IMovieSeenAppService
     {
         private readonly IRepository<Movie_Seen> _MovieSeenRepository;
+        private readonly IRepository<Movie> _MovieRepository;
+        private readonly IRepository<Gender> _GenderRepository;
 
-        public MovieSeenAppService(IRepository<Movie_Seen> movieSeenRepository) : base(movieSeenRepository)
+        public MovieSeenAppService(IRepository<Movie_Seen> movieSeenRepository, IRepository<Movie> movieRepository
+            , IRepository<Gender> genderRepository) 
+            : base(movieSeenRepository)
         {
             _MovieSeenRepository = movieSeenRepository;
+            _MovieRepository = movieRepository;
+            _GenderRepository = genderRepository;
         }
 
         public override async Task<Movie_SeenDto> CreateAsync(CreateMovie_SeenDto input)
@@ -67,5 +75,83 @@ namespace PeliFlix.Movies_Seen
             var movieSeen = await _MovieSeenRepository.GetAsync(input.Id);
             await _MovieSeenRepository.DeleteAsync(movieSeen);   
         }
+
+        //public async Task<ListResultDto<GetAllMovieInfoDto>> GetInfoMovie(int idMovie_Seen, int idMovie)
+        //{
+        //    var movieInfoDto = new GetAllMovieInfoDto();
+
+        //    var getInfoMovie = await _MovieRepository.FirstOrDefaultAsync(m => m.Id == idMovie);
+        //    var getInfoMovieSeen = await _MovieSeenRepository.FirstOrDefaultAsync(ms => ms.Id == idMovie_Seen);
+
+        //    if (getAllInfoMovie == null || getInfoMovieSeen == null)
+        //    {
+        //        throw new UserFriendlyException("Sorry, this movie dosen´t found");
+        //    }
+
+        //    movieInfoDto.TitleMovie = getInfoMovie.Title;
+        //    movieInfoDto.SynopsisMovie = getInfoMovie.Synopsis;
+        //    movieInfoDto.DirectorMovie = getInfoMovie.Director;
+        //    movieInfoDto.MovieYear = getInfoMovie.year;
+
+        //    movieInfoDto.ViewDate = getInfoMovieSeen.View_Date;
+        //    movieInfoDto.Score = getInfoMovieSeen.Score;
+
+        //    return new ListResultDto<GetAllMovieInfoDto>(ObjectMapper.Map<List<GetAllMovieInfoDto>>(movieInfoDto));
+        //}
+
+        public async Task<Movie_SeenDto> GetInfoMovieSeen(int IdMovieSeen, int idMovie)
+        {
+            var movieInfoDto = new Movie_SeenDto();
+
+            var getInfoMovie = await _MovieRepository.FirstOrDefaultAsync(m => m.Id == idMovie);
+            var getInfoMovieSeen = await _MovieSeenRepository.FirstOrDefaultAsync(ms => ms.Id == IdMovieSeen);
+
+            if (getInfoMovie == null || getInfoMovieSeen == null)
+            {
+                throw new UserFriendlyException("Sorry, this movie dosen´t found");
+            }
+
+            movieInfoDto.TitleMovie = getInfoMovie.Title;
+            movieInfoDto.SynopsisMovie = getInfoMovie.Synopsis;
+            movieInfoDto.DirectorMovie = getInfoMovie.Director;
+            movieInfoDto.MovieYear = getInfoMovie.year;
+            movieInfoDto.MovieId = getInfoMovie.Id;            
+
+            movieInfoDto.viewDate = getInfoMovieSeen.View_Date;
+            movieInfoDto.Score = getInfoMovieSeen.Score;
+            movieInfoDto.UserId = getInfoMovieSeen.UserId;
+
+            return movieInfoDto;
+        }
+
+
+
+        public async Task<ListResultDto<Movie_SeenDto>> GetAllMoviesAsync()
+        {
+
+            var completMovieInfo = from ms in await _MovieSeenRepository.GetAllListAsync()
+                                   join m in await _MovieRepository.GetAllListAsync()
+                                   on ms.MovieId equals m.Id
+                                   join g in await _GenderRepository.GetAllListAsync()
+                                   on m.GenderId equals g.Id
+                                    select new Movie_SeenDto
+                                    {
+                                        TitleMovie = m.Title,
+                                        SynopsisMovie = m.Synopsis,
+                                        DirectorMovie = m.Director,
+                                        MovieYear = m.year,
+                                        MovieId = m.Id,
+                                        GenderMovie = g.Name,
+
+                                        Id = ms.Id,
+                                        UserId = ms.UserId,
+                                        View = ms.View,
+                                        viewDate = ms.View_Date,
+                                        Score = ms.Score
+                                    };
+            return new ListResultDto<Movie_SeenDto>(ObjectMapper.Map<List<Movie_SeenDto>>(completMovieInfo));            
+            
+        }
+         
     }
 }
