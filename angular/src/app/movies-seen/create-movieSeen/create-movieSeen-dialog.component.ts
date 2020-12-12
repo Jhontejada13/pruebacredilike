d3ddf03905e8a7d97404fb450fa599c9b67a09f7
Jhon Tejada <jhontejada13@gmail.com>
2020-12-12 18:20:03 -0500

    Se realiza el código para mostrar el modal y la funcionalidad para guardar, se debe solucionar erro con fecha

--- angular/src/app/movies-seen/create-movieSeen/create-movieSeen-dialog.component.ts
+++ angular/src/app/movies-seen/create-movieSeen/create-movieSeen-dialog.component.ts
@@ -7,12 +7,15 @@ import {
 } from '@angular/core';
 import { finalize } from 'rxjs/operators';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { forEach as _forEach, map as _map } from 'lodash-es';
 import { AppComponentBase } from '@shared/app-component-base';
 import {
     MovieSeenServiceProxy,
     CreateMovie_SeenDto,
+    MovieDto,
 } from '@shared/service-proxies/service-proxies';
+import { AbpSessionService} from 'abp-ng2-module';
 
 @Component({
     templateUrl: './create-movieSeen-dialog.component.html',
@@ -21,35 +24,47 @@ import {
 export class CreateMovieSeenDialogComponent extends AppComponentBase implements OnInit {
     saving = false;
     movieSeen = new CreateMovie_SeenDto();
+    movie = new MovieDto();
+    movieId: number;
+    
 
     @Output() onSave = new EventEmitter<any>();
 
     constructor(
         injector: Injector,
         public _movieSeenService: MovieSeenServiceProxy,
-        public bsModalRef: BsModalRef
+        public bsModalRef: BsModalRef,
+        public _sesionService: AbpSessionService
     ) {
         super(injector);
     }
 
     ngOnInit(): void {
-
+        console.log(this.movieId);
     }
 
     save(): void {
         this.saving = true
 
-        this._movieSeenService
-            .create(this.movieSeen)
-            .pipe(
-                finalize(() => {
-                    this.saving = false;
-                })
-            )
-            .subscribe(() => {
-                this.notify.info(this.l('SavedSuccessfully'));
-                this.bsModalRef.hide();
-                this.onSave.emit();
-            });
+
+        this.movieSeen.userId = this._sesionService.userId
+        this.movieSeen.view = true;
+        this.movieSeen.movieId = this.movieId;
+
+        console.log(this.movieSeen.viewDate);
+        console.log(typeof(this.movieSeen.viewDate));
+
+         /*this._movieSeenService
+             .create(this.movieSeen)
+             .pipe(
+                 finalize(() => {
+                     this.saving = false;
+                 })
+             )
+             .subscribe(() => {
+                 this.notify.info(this.l('SavedSuccessfully'));
+                 this.bsModalRef.hide();
+                 this.onSave.emit();
+             });*/
     }
 }
